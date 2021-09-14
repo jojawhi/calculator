9786b57866819066367c8a9eeec4966700a856dc
Josh White <jojawhi@gmail.com>
2021-09-13 17:50:58 -0700

    added basic calculation functionality to oneButton and operator buttons, need to add functionality to clear the result when starting a new equation

--- calc.js
+++ calc.js
@@ -1,5 +1,6 @@
 const displayContainer = document.querySelector("#displayContainer");
 const displayEquation = document.querySelector("#displayEquation");
+const displayResult = document.querySelector("#displayResult");
 const zeroButton = document.querySelector("#zeroButton");
 const oneButton = document.querySelector("#oneButton");
 const twoButton = document.querySelector("#twoButton");
@@ -20,18 +21,57 @@ const diviButton = document.querySelector("#diviButton");
 const equButton = document.querySelector("#equButton");
 
 displayEquation.textContent = "";
+displayResult.textContent = "";
 
+
+let firstOperand = "";
+
+let secondOperand = "";
+
+let result = "";
+
+let addClicked = false;
+let subClicked = false;
+let multClicked = false;
+let diviClicked = false;
 
 zeroButton.addEventListener("click", () => {
-    displayEquation.textContent += "0";
+    
+    if (firstOperand != "") {
+        secondOperand += "0";
+        displayEquation.textContent += "0";
+    } else {
+        displayEquation.textContent += "0";
+    }
+    
 });
 
 oneButton.addEventListener("click", () => {
-    displayEquation.textContent += "1";
+
+    if (typeof result === Number) {
+        displayEquation.textContent = "";
+    }
+
+    if (firstOperand != "") {
+        secondOperand += "1";
+        displayEquation.textContent += "1";
+    } else {
+        displayEquation.textContent += "1";
+    }
+
+   
+
 });
 
 twoButton.addEventListener("click", () => {
-    displayEquation.textContent += "2";
+
+    if (firstOperand != "") {
+        secondOperand += "2";
+        displayEquation.textContent += "2";
+    } else {
+        displayEquation.textContent += "2";
+    }
+
 });
 
 threeButton.addEventListener("click", () => {
@@ -67,25 +107,64 @@ pointButton.addEventListener("click", () => {
 });
 
 addButton.addEventListener("click", () => {
+    addClicked = true;
+    firstOperand = Number(displayEquation.textContent);
     displayEquation.textContent += "+";
+    console.log(firstOperand);
 })
 
 subButton.addEventListener("click", () => {
+    subClicked = true;
+    firstOperand = Number(displayEquation.textContent);
     displayEquation.textContent += "-";
 })
 
 multButton.addEventListener("click", () => {
+    multClicked = true;
+    firstOperand = Number(displayEquation.textContent);
     displayEquation.textContent += "*";
 })
 
 diviButton.addEventListener("click", () => {
+    diviClicked = true;
+    firstOperand = Number(displayEquation.textContent);
     displayEquation.textContent += "/";
 })
 
 equButton.addEventListener("click", () => {
-    displayEquation.textContent += "=";
+    displayEquation.textContent += " =";
+
+    if (addClicked === true) {
+        result = Number(firstOperand) + Number(secondOperand);
+    } else if (subClicked === true) {
+        result = Number(firstOperand) - Number(secondOperand);
+    } else if (multClicked === true) {
+        result = Number(firstOperand) * Number(secondOperand);
+    } else if (diviClicked === true) {
+        result = Number(firstOperand) / Number(secondOperand);
+    }
+
+    displayResult.textContent = result;
+
+    console.log(typeof result);
+
+    addClicked = false;
+    subClicked = false;
+    multClicked = false;
+    diviClicked = false;
+    
 })
 
 clearButton.addEventListener("click", () => {
     displayEquation.textContent = "";
+    displayResult.textContent = ""
+
+    firstOperand = "";
+    secondOperand = "";
+
+    addClicked = false;
+    subClicked = false;
+    multClicked = false;
+    diviClicked = false;
+    
 })
